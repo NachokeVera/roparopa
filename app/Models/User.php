f5134e9ef82c6e9d8681d745a111d1b2c1d9e37e
NachokeVera <ignacio.vera.hidalgo@gmail.com>
2023-10-03 00:25:42 -0300

    RegisterRequest, User models, y la migracion

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,9 +18,13 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
-        'email',
+        'rut',
+        'nombre',
+        'apellido',
+        'correo',
         'password',
+        'direccion',
+        'telefono'
     ];
 
     /**
